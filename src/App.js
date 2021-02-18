59534755882c33f4ca7c2eb66c4ba40ad10f9841
Sangpil Jung <tankteam20@gmail.com>
2021-02-18 20:15:07 +1000

    add component and handle event

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React, { useState } from 'react';
 import { MdAddCircle } from 'react-icons/md';
 import './App.css';
 import Template from './components/Template';
+import TodoInsert from './components/Todoinsert';
 import TodoList from './components/TodoList';
 
+
+let nextID = 4;
 function App() {
+  
+  const [insertToggle, setInsertToggle] = useState(false);
+
   const [todos,setTodos] = useState([
     {
       id:1,
@@ -16,17 +22,41 @@ function App() {
       text: "todo 2",
       checked: false
     },
-    {
+    { 
       id:3,
       text: "todo 3",
       checked: true
     },
     
   ])
+
+  const onInsertToggle = () => {
+    setInsertToggle(prev => !prev);
+  };
+
+  const onInsertTodo = (text) => {
+    if(text === ""){
+      return alert('please type todos')
+    }else{
+      const todo = {
+        id: nextID,
+        text,
+        checked: false 
+      }
+      setTodos(todos => todos.concat(todo));
+      nextID++;
+    }
+  };
+
+  const onCheckToggle = (id) => {
+    setTodos(todos => todos.map(todo => (todo.id === id ? {...todo, checked: !todo.checked}: todo)))
+  }
+
   return (
     <Template todoLength={todos.length}>
-      <TodoList todos={todos}/>
-      <div className="add-todo-button"><MdAddCircle/></div>
+      <TodoList todos={todos} onCheckToggle={onCheckToggle}/>
+      <div className="add-todo-button" onClick={onInsertToggle}><MdAddCircle/></div>
+      {insertToggle && <TodoInsert onInsertToggle={onInsertToggle} onInsertTodo={onInsertTodo}/>}
     </Template>
     
   );
